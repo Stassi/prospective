6a7543cba32af6b8127398c625d36538bbd5a056
Andreas Stassivik <andreas@stassi.net>
2019-03-01 01:23:30 -0800

    +CssBaseline

--- components/NextHead/NextHead.js
+++ components/NextHead/NextHead.js
@@ -1,20 +1,24 @@
+import CssBaseline from '@material-ui/core/CssBaseline'
 import Head from 'next/head'
 import React from 'react'
 import MaterialStyles from './MaterialStyles'
 
 export default ({
+  baseline = true,
   fontIcons = false,
   responsive = true,
   roboto = true,
   title = 'App'
-}) => (
-  <Head>
+}) => [
+  <Head key='head'>
     <title>{title}</title>
 
     <MaterialStyles {...{
+      baseline,
       fontIcons,
       responsive,
       roboto
     }} />
-  </Head>
-)
+  </Head>,
+  baseline && <CssBaseline key='baseline' />
+]
