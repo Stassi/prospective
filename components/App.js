2cd841a1c482b86924c203f089cbef17388312a2
Andreas Stassivik <andreas@stassi.net>
2019-03-01 00:37:00 -0800

    fragment fixes rendering issue

--- components/App.js
+++ components/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Button from '@material-ui/core/Button'
 import NextHead from './NextHead'
 
-export default () => [
-  <NextHead />,
-  <Button
-    color='primary'
-    variant='contained'
-  >
-    Hello World
-  </Button>
-]
+export default () => (
+  <>
+    <NextHead />
+    <Button
+      color='primary'
+      variant='contained'
+    >
+      Hello World
+    </Button>
+  </>
+)
