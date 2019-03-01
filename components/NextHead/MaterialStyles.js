a9a3e92061b2322207d2f4f83a800c7939661970
Andreas Stassivik <andreas@stassi.net>
2019-03-01 00:52:01 -0800

    +keys

--- components/NextHead/MaterialStyles.js
+++ components/NextHead/MaterialStyles.js
@@ -4,12 +4,14 @@ export default ({ fontIcons, roboto }) => [
   fontIcons && (
     <link
       href='https://fonts.googleapis.com/icon?family=Material+Icons'
+      key='fontIcons'
       rel='stylesheet'
     />
   ),
   roboto && (
     <link
       href='https://fonts.googleapis.com/css?family=Roboto:300,400,500'
+      key='roboto'
       rel='stylesheet'
     />
   )
