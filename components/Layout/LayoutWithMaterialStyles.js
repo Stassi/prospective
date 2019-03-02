f48bffa0dd6389d11ff5fc9723d715f284153e4d
Andreas Stassivik <andreas@stassi.net>
2019-03-01 16:37:00 -0800

    named HOC reveals side effect

--- components/Layout/LayoutWithMaterialStyles.js
+++ components/Layout/LayoutWithMaterialStyles.js
@@ -1,8 +1,11 @@
+import React from 'react'
 import { install } from '@material-ui/styles'
 import Layout from './Layout'
 
 // attempts to load style engine before importing styled Material UI components
 // reference: https://material-ui.com/css-in-js/basics/
-install()
+const LayoutWithMaterialStyles = ({ ...props }) => install() || (
+  <Layout {...props} />
+)
 
-export default Layout
+export default LayoutWithMaterialStyles
