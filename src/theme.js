405247c4ab094fbd3172256d23862891958e6c74
Andreas Stassivik <andreas@stassi.net>
2019-05-24 19:35:33 -0700

    formatting

--- src/theme.js
+++ src/theme.js
@@ -1,20 +1,19 @@
 import { createMuiTheme } from '@material-ui/core/styles'
 import red from '@material-ui/core/colors/red'
 
-// Create a theme instance.
 const theme = createMuiTheme({
   palette: {
+    background: {
+      default: '#fff'
+    },
+    error: {
+      main: red.A400
+    },
     primary: {
       main: '#556cd6'
     },
     secondary: {
       main: '#19857b'
-    },
-    error: {
-      main: red.A400
-    },
-    background: {
-      default: '#fff'
     }
   }
 })
