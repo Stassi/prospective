0bf6e75fa94f50ff7234eef98992128c4e59c90d
Andreas Stassivik <andreas@stassi.net>
2019-05-24 19:48:48 -0700

    formatting, boilerplate cleanup

--- pages/_document.js
+++ pages/_document.js
@@ -43,13 +43,16 @@ MyDocument.getInitialProps = async ctx => {
   const sheets = new ServerStyleSheets()
   const originalRenderPage = ctx.renderPage
 
-  ctx.renderPage = () => originalRenderPage({ enhanceApp: App => props => sheets.collect(<App {...props} />) })
+  ctx.renderPage = () => originalRenderPage({
+    enhanceApp: App => props => sheets.collect(
+      <App {...props} />
+    )
+  })
 
   const initialProps = await Document.getInitialProps(ctx)
 
   return {
     ...initialProps,
-    // Styles fragment is rendered after the app and page rendering finish.
     styles: (
       <>
         {sheets.getStyleElement()}
