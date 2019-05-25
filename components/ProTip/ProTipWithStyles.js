53088432489afb5a69ea1a807305d4bd7f86877c
Andreas Stassivik <andreas@stassi.net>
2019-05-25 03:48:57 -0700

    argument deconstruction

--- components/ProTip/ProTipWithStyles.js
+++ components/ProTip/ProTipWithStyles.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import ProTip from './ProTip'
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles(({ spacing }) => ({
   lightBulb: {
-    marginRight: theme.spacing(1),
+    marginRight: spacing(1),
     verticalAlign: 'middle'
   },
   root: {
-    margin: theme.spacing(6, 0, 3)
+    margin: spacing(6, 0, 3)
   }
 }))
 
