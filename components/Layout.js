6e548afa28a52bd09c14c6badb0af9801872a4fa
Andreas Stassivik <andreas@stassi.net>
2019-06-02 15:40:16 -0700

    +Layout

---
+++ components/Layout.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ThemeButton from './ThemeButton'
+
+const Layout = ({ toggleDarkTheme }) => (
+  <ThemeButton {...{ toggleDarkTheme }} />
+)
+
+export default Layout
