b510bc7bfd7f919fa4f4f389ceb2a1b80bf7834e
Andreas Stassivik <andreas@stassi.net>
2019-06-03 02:52:02 -0700

    +implausible algorithms

---
+++ src/implausibleAlgorithms.js
@@ -0,0 +1,11 @@
+const implausibleAlgorithms = [
+  { label: 'Alea', value: 'alea' },
+  { label: 'Arc4', value: 'arc4' },
+  { label: 'Tychei', value: 'tychei' },
+  { label: 'Xor128', value: 'xor128' },
+  { label: 'Xor4096', value: 'xor4096' },
+  { label: 'Xorshift7', value: 'xorshift7' },
+  { label: 'Xorwow', value: 'xorwow' }
+]
+
+export default implausibleAlgorithms
