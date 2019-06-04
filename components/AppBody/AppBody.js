6a2404657fe47b4458e60f59348b40750c85142d
Andreas Stassivik <andreas@stassi.net>
2019-06-03 17:49:20 -0700

    +propTypes

--- components/AppBody/AppBody.js
+++ components/AppBody/AppBody.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { ThemeProvider } from '@material-ui/styles'
 import theme from '../../src/theme'
+
+const propTypes = {
+  Component: PropTypes.func.isRequired,
+  darkTheme: PropTypes.shape({
+    enabled: PropTypes.bool.isRequired,
+    toggle: PropTypes.func.isRequired
+  }).isRequired
+}
 
 const AppBody = ({
   Component,
@@ -19,5 +28,7 @@ const AppBody = ({
     />
   </ThemeProvider>
 )
+
+AppBody.propTypes = propTypes
 
 export default AppBody
