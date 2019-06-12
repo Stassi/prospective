66b2b38c89cd54f7205fa7086b7dc0759c211256
Andreas Stassivik <andreas@stassi.net>
2019-06-12 02:57:32 -0700

    +(seed adornment)

--- components/DebugContainer/SeedTextField.js
+++ components/DebugContainer/SeedTextField.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import IconButton from '@material-ui/core/IconButton'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import LocalFlorist from '@material-ui/icons/LocalFlorist'
 import TextField from '../TextField'
+
+const InputProps = {
+  startAdornment: (
+    <InputAdornment position='start'>
+      <IconButton>
+        <LocalFlorist />
+      </IconButton>
+    </InputAdornment>
+  )
+}
 
 const SeedTextField = props => (
   <TextField
+    {...{ InputProps }}
     id='seed'
     label='Seed'
     type='search'
