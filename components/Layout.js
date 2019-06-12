ca8e3a14b5ad567833e2de68a3014a9d402a583f
Andreas Stassivik <andreas@stassi.net>
2019-06-11 17:07:14 -0700

    +SeedTextField

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import AlgorithmMenuSelect from './AlgorithmMenuSelect'
 import AppBar from './AppBar'
+import SeedTextField from './SeedTextField'
 import ThemeButton from './ThemeButton'
 
 const Layout = ({ toggleDarkTheme }) => (
   <AppBar>
+    <SeedTextField />
     <AlgorithmMenuSelect />
     <ThemeButton {...{ toggleDarkTheme }} />
   </AppBar>
---
+++ components/SeedTextField.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import TextField from './TextField'
+
+const SeedTextField = props => (
+  <TextField
+    id='seed'
+    label='Seed'
+    type='search'
+    {...props}
+  />
+)
+
+export default SeedTextField
