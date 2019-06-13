cc6b2417b0d960e8a04e18d200cb7d551d5bc85c
Andreas Stassivik <andreas@stassi.net>
2019-06-13 11:42:36 -0700

    +TODO

--- components/DebugOuterContainer.js
+++ components/DebugOuterContainer.js
@@ -1,5 +1,6 @@
 import { debugOuterContainer as enhance } from '../src/enhancers'
 
+// TODO: Rename
 const DebugOuterContainer = ({ children, ...props }) => children(props)
 
 const EnhancedDebugOuterContainer = enhance(DebugOuterContainer)
